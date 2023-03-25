287b73491efca2818ad26d6a7289bca64e570dea
enpitsulin <enpitsulin@gmail.com>
2023-03-25 14:41:37 +0800

    feat: fab hidden in right side

--- src/components/FloatActionButton.vue
+++ src/components/FloatActionButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="fixed right-4 bottom-4">
+  <div
+    ref="fab"
+    class="fixed right-3 bottom-4 transition-transform ease-out"
+    :class="isOutside ? 'translate-x-full' : 'translate-x-0'"
+  >
     <div
       class="bg-$ud-bg text-$ud-text transition-all shadow-md overflow-hidden rounded-md cursor-pointer flex"
       :class="modelValue ? 'translate-x-[calc(100%_+_1rem)]' : 'translate-x-0'"
@@ -13,4 +17,7 @@
 <script setup lang="ts">
   defineProps<{ modelValue: boolean }>();
   defineEmits<{ (e: 'update:modelValue', p: boolean): void }>();
+
+  const fab = ref(null);
+  const { isOutside } = useMouseInElement(fab);
 </script>
