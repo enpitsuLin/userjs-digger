d9f3ba2ba5b6becf1a45f30cb3ef281a0e4df4b9
enpitsulin <enpitsulin@gmail.com>
2023-03-26 21:48:09 +0800

    feat: add setting type definition

--- src/composables/app.ts
+++ src/composables/app.ts
@@ -1,6 +1,10 @@
 export const useInjectContainer = () => inject<HTMLDivElement>('container')!;
 
+const defaultSettings: UserjsDiggerSettings = {
+  locale: navigator.language ?? 'en',
+  nsfw: false,
+  filter: '',
+  debugger: false
+};
 export const useUserjsDiggerSettings = () =>
-  useGMStorage('ud_settings', {
-    locale: navigator.language ?? 'en'
-  });
+  useGMStorage('ud_settings', defaultSettings);
---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+declare type UserjsDiggerSettings = {
+  /** language */
+  locale: string;
+  /** add Sleazyfork result */
+  nsfw: boolean;
+  /** filtered site */
+  filter: string;
+  /** debugger */
+  debugger: boolean;
+};
