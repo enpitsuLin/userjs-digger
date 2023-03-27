c1f9e5da3829b19b4596ef7f1b4e60ce49023dce
enpitsulin <enpitsulin@gmail.com>
2023-03-27 22:07:10 +0800

    feat: TagsInput as component

--- src/components/SettingsPanel.vue
+++ src/components/SettingsPanel.vue
@@ -48,33 +48,7 @@
             {{ t('filter-desc') }}
           </p>
         </div>
-        <div
-          class="p-2 gap-y-1 inline-flex flex-wrap relative shadow-sm border-1px border-$ud-border rounded-md focus-within:ring-2 focus-within:ring-indigo-500 focus-within:border-indigo-500"
-        >
-          <div
-            v-for="(item, i) of settings.filter"
-            class="inline-flex mr-1 rounded-full items-center py-0.5 pl-2.5 pr-1 text-xs font-medium bg-indigo-100 text-indigo-700"
-          >
-            <span>
-              {{ item }}
-            </span>
-            <button
-              type="button"
-              class="flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:outline-none focus:bg-indigo-500 focus:text-white"
-              @click="settings.filter.splice(i)"
-            >
-              <div class="sr-only">Remove</div>
-              <div class="h-3 w-3 i-carbon-close"></div>
-            </button>
-          </div>
-          <input
-            v-model="inputVal"
-            type="text"
-            class="bg-transparent border-none ring-none! flex-1 p-0 text-xs"
-            @keypress.enter="onFilterAdd"
-            @keydown.backspace="onFilterBack"
-          />
-        </div>
+        <TagsInput v-model:tags="settings.filter" />
       </li>
     </ul>
   </div>
@@ -93,16 +67,4 @@
   const enable = useSessionStorage('ud_show', true);
 
   const { t } = useI18n();
-
-  const inputVal = ref('');
-  const onFilterAdd = () => {
-    if (inputVal.value !== '') {
-      settings.value.filter.push(inputVal.value);
-      inputVal.value = '';
-    }
-  };
-  const onFilterBack = useThrottleFn(() => {
-    if (settings.value.filter.length > 0 && inputVal.value === '')
-      settings.value.filter.splice(settings.value.filter.length - 1);
-  }, 500);
 </script>
