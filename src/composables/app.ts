40eb164a2d0e8a07be9b1069f75482e4f9f04722
enpitsulin <enpitsulin@gmail.com>
2023-03-27 22:22:30 +0800

    fix: reset setting for break change maybe

--- src/composables/app.ts
+++ src/composables/app.ts
@@ -6,5 +6,21 @@ const defaultSettings: UserjsDiggerSettings = {
   filter: [],
   debugger: false
 };
-export const useUserjsDiggerSettings = () =>
-  useGMStorage('ud_settings', defaultSettings);
+
+const toString = Object.prototype.toString;
+
+export const useUserjsDiggerSettings = () => {
+  const settings = useGMStorage('ud_settings', defaultSettings);
+
+  Object.entries(settings.value).forEach(([key, value]) => {
+    if (
+      toString.call(value) !==
+      toString.call(defaultSettings[key as keyof UserjsDiggerSettings])
+    ) {
+      //@ts-expect-error
+      settings.value[key as keyof UserjsDiggerSettings] =
+        defaultSettings[key as keyof UserjsDiggerSettings];
+    }
+  });
+  return settings;
+};
