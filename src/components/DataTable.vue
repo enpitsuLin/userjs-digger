d86ea6f0e5c3785ec5d68a625f2dc816353856b8
enpitsulin <enpitsulin@gmail.com>
2023-03-30 10:06:17 +0800

    chore: move formatTimeAgo to utils folder

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -171,7 +171,7 @@
 </template>
 
 <script setup lang="ts">
-  import { formatTimeAgo, UseTimeAgoUnitNamesDefault } from '@vueuse/core';
+  import { formatTimeAgoWithI18n } from '../utils/date';
   import { GreasyforkScript } from '../composables/data';
 
   const props = defineProps<{ data: GreasyforkScript[]; loading: boolean }>();
@@ -224,42 +224,4 @@
   };
 
   const { t } = useI18n();
-
-  const formatTimeAgoWithI18n = (from: Date) => {
-    return formatTimeAgo<UseTimeAgoUnitNamesDefault>(from, {
-      messages: {
-        justNow: t('time-ago.just-now'),
-        past: (n) => (n.match(/\d/) ? t('time-ago.past', { n }) : n),
-        future: (n) => (n.match(/\d/) ? t('time-ago.future', { n }) : n),
-        month: (n, past) =>
-          n === 1
-            ? past
-              ? t('time-ago.month.past')
-              : t('time-ago.month.future')
-            : t('time-ago.month.n', { n }),
-        year: (n, past) =>
-          n === 1
-            ? past
-              ? t('time-ago.year.past')
-              : t('time-ago.year.future')
-            : t('time-ago.year.n', { n }),
-        day: (n, past) =>
-          n === 1
-            ? past
-              ? t('time-ago.day.past')
-              : t('time-ago.day.future')
-            : t('time-ago.day.n', { n }),
-        week: (n, past) =>
-          n === 1
-            ? past
-              ? t('time-ago.week.past')
-              : t('time-ago.week.future')
-            : t('time-ago.week.n', { n }),
-        hour: (n) => t('time-ago.hour', { n }),
-        minute: (n) => t('time-ago.minute', { n }),
-        second: (n) => t('time-ago.second', { n }),
-        invalid: 'invalid'
-      }
-    });
-  };
 </script>
---
+++ src/utils/date.ts
@@ -0,0 +1,40 @@
+import { formatTimeAgo, UseTimeAgoUnitNamesDefault } from '@vueuse/core';
+
+export const formatTimeAgoWithI18n = (from: Date) => {
+  const { t } = useI18n();
+  return formatTimeAgo<UseTimeAgoUnitNamesDefault>(from, {
+    messages: {
+      justNow: t('time-ago.just-now'),
+      past: (n) => (n.match(/\d/) ? t('time-ago.past', { n }) : n),
+      future: (n) => (n.match(/\d/) ? t('time-ago.future', { n }) : n),
+      month: (n, past) =>
+        n === 1
+          ? past
+            ? t('time-ago.month.past')
+            : t('time-ago.month.future')
+          : t('time-ago.month.n', { n }),
+      year: (n, past) =>
+        n === 1
+          ? past
+            ? t('time-ago.year.past')
+            : t('time-ago.year.future')
+          : t('time-ago.year.n', { n }),
+      day: (n, past) =>
+        n === 1
+          ? past
+            ? t('time-ago.day.past')
+            : t('time-ago.day.future')
+          : t('time-ago.day.n', { n }),
+      week: (n, past) =>
+        n === 1
+          ? past
+            ? t('time-ago.week.past')
+            : t('time-ago.week.future')
+          : t('time-ago.week.n', { n }),
+      hour: (n) => t('time-ago.hour', { n }),
+      minute: (n) => t('time-ago.minute', { n }),
+      second: (n) => t('time-ago.second', { n }),
+      invalid: 'invalid'
+    }
+  });
+};
