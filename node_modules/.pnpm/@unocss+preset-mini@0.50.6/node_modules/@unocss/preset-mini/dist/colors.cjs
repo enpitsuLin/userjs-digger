77daef0a8816ccff7bd19287ed28715a000917f5
enpitsulin <enpitsulin@gmail.com>
2023-04-04 15:54:22 +0800

    chore: remove added node_modules

--- node_modules/.pnpm/@unocss+preset-mini@0.50.6/node_modules/@unocss/preset-mini/dist/colors.cjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const colors = require('./shared/preset-mini.30606736.cjs');
-
-
-
-exports.colors = colors.colors;
